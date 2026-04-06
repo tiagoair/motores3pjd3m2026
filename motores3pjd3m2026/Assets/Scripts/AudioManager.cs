017ff60cf490f5222e4783d445da65c9ddcda1e0
Tiago Sousa <tiagoair@gmail.com>
2026-04-06 11:54:25 -0300

    Add system audio channels and 2D playback APIs
    Introduce systemSourceChannels and activeSources lists and initialize them in Awake. Add APIs for 2D audio: PlayMusic/StopMusic/PauseMusic/ResumeMusic, PlayOneShot, and PlayAmbient/StopAmbient/PauseAmbient/ResumeAmbient. AudioSource components are created on demand (index 0 used for music/one-shot, index 1 for ambient). Organize code with regions and keep activeSources reserved for future use.

--- motores3pjd3m2026/Assets/Scripts/AudioManager.cs
+++ motores3pjd3m2026/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,17 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    #region Private Fields
+
+    private List<AudioSource> systemSourceChannels;
+    private List<AudioSource> activeSources;
+    
+    #endregion
+    
+    #region Singleton
     public static AudioManager Instance;
 
     private void Awake()
@@ -11,10 +20,97 @@ public class AudioManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            systemSourceChannels = new List<AudioSource>();
+            activeSources = new List<AudioSource>();
         }
         else
         {
             Destroy(gameObject);
         }
     }
+    #endregion
+    
+    #region Play 2D Sounds
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (systemSourceChannels.Count == 0)
+        {
+            systemSourceChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+        systemSourceChannels[0].Stop();
+        systemSourceChannels[0].clip = clip;
+        systemSourceChannels[0].Play();
+    }
+
+    public void StopMusic()
+    {
+        if (systemSourceChannels.Count > 0)
+        {
+            systemSourceChannels[0].Stop();
+        }
+    }
+
+    public void PauseMusic()
+    {
+        if (systemSourceChannels.Count > 0)
+        {
+            systemSourceChannels[0].Pause();
+        }
+    }
+    
+    public void ResumeMusic()
+    {
+        if (systemSourceChannels.Count > 0)
+        {
+            systemSourceChannels[0].UnPause();
+        }
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        if (systemSourceChannels.Count == 0)
+        {
+            systemSourceChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+        systemSourceChannels[0].PlayOneShot(clip);
+    }
+    
+    public void PlayAmbient(AudioClip clip)
+    {
+        while (systemSourceChannels.Count < 2)
+        {
+            systemSourceChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+        systemSourceChannels[1].Stop();
+        systemSourceChannels[1].clip = clip;
+        systemSourceChannels[1].Play();
+    }
+
+    public void StopAmbient()
+    {
+        if (systemSourceChannels.Count >= 2)
+        {
+            systemSourceChannels[1].Stop();
+        }
+    }
+
+    public void PauseAmbient()
+    {
+        if (systemSourceChannels.Count >= 2)
+        {
+            systemSourceChannels[1].Pause();
+        }
+    }
+    
+    public void ResumeAmbient()
+    {
+        if (systemSourceChannels.Count >= 2)
+        {
+            systemSourceChannels[1].UnPause();
+        }
+    }
+    #endregion
+    
+    
 }
